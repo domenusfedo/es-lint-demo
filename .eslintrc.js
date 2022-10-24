bc44ea3a85483fec9b189fffadf6c1e9d007ce04
Dominik Globisz <dominik.globisz@noaignite.com>
2022-10-24 14:34:03 +0200

    Airbnb Test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
 module.exports = {
   env: {
     browser: true,
+    node: true,
     es2021: true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb-typescript",
-    "next/core-web-vitals",
-    "prettier",
-  ],
+  extends: ['airbnb-base', 'next/core-web-vitals', 'prettier'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "no-console": ["warn", { allow: ["warn", "error"] }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
   },
 };
